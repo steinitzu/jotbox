0413b3d9c0bc40b5c2e8e31104694d9b44de5727
Steinthor Palsson <steini90@gmail.com>
2024-02-07 17:30:42 -0500

    Update python version, mypy, use redis-py

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -5,10 +5,8 @@ import json
 from unittest import mock
 
 import pytest
-import asyncmock
-import jwt
 
-from jotbox import Jotbox, JWTDecodeError, RevokedTokenError
+from jotbox import Jotbox, JWTDecodeError, RevokedTokenError, Payload
 from jotbox import tokens
 
 KEY = token_urlsafe()
@@ -18,7 +16,7 @@ class TestCreatePayload:
     @pytest.mark.asyncio
     async def test__exp_is_added(self):
         expires_in = 4400
-        jot = Jotbox(encode_key=KEY, expires_in=4400)
+        jot = Jotbox[Payload](encode_key=KEY, expires_in=4400)
 
         payload = jot.create_payload()
 
@@ -28,7 +26,7 @@ class TestCreatePayload:
 class TestVerifiedPayload:
     @pytest.mark.asyncio
     async def test__args_passed_to_pyjwt(self):
-        jot = Jotbox(
+        jot = Jotbox[Payload](
             encode_key=KEY,
             expires_in=4400,
             leeway=30,
@@ -54,7 +52,7 @@ class TestVerifiedPayload:
 
     @pytest.mark.asyncio
     async def test__idle_timeout__calls_whitelist_with_until(self, redis_whitelist):
-        jot = Jotbox(
+        jot = Jotbox[Payload](
             encode_key=KEY,
             expires_in=4400,
             leeway=30,
@@ -68,30 +66,31 @@ class TestVerifiedPayload:
         token = await jot.create_token(payload)
 
         start = time.time()
-        with asyncmock.patch.object(
+        with mock.patch.object(
             redis_whitelist, "touch", autospec=True, return_value=payload_raw
         ) as m:
             _ = await jot.verified_payload(token.token)
         now = time.time()
-        until = m.await_args[0][1]
+        until = m.await_args[0][1]  # type: ignore[index]
 
         m.assert_awaited_once()
+        assert jot.idle_timeout is not None
         assert until >= start + jot.idle_timeout + jot.leeway
         assert until <= now + jot.idle_timeout + jot.leeway
 
     @pytest.mark.asyncio
     async def test__pyjwt_error__raises_jwt_decode_error(self):
-        jot = Jotbox(encode_key=KEY, expires_in=-1)
+        jot = Jotbox[Payload](encode_key=KEY, expires_in=-1)
         token = await jot.create_token()
 
         with pytest.raises(JWTDecodeError):
-            await jot.verified_payload(token)
+            await jot.verified_payload(token.token)
 
     @pytest.mark.asyncio
     async def test__revoked_whitelist__raises_revoked_token_error(
         self, redis_whitelist
     ):
-        jot = Jotbox(encode_key=KEY, expires_in=30, whitelist=redis_whitelist)
+        jot = Jotbox[Payload](encode_key=KEY, expires_in=30, whitelist=redis_whitelist)
         token = await jot.create_token()
 
         await jot.revoke_payload(token.payload)
@@ -103,46 +102,48 @@ class TestVerifiedPayload:
 class TestAddToWhitelist:
     @pytest.mark.asyncio
     async def test__idle_timeout_no_exp__until_correct(self, redis_whitelist):
-        jot = Jotbox(
+        jot = Jotbox[Payload](
             encode_key=KEY, leeway=10, idle_timeout=3600, whitelist=redis_whitelist
         )
         payload = jot.create_payload()
 
         start = time.time()
-        with asyncmock.patch.object(
+        with mock.patch.object(
             jot.whitelist, "add", autospec=True, return_value=None
         ) as m:
             await jot.add_to_whitelist(payload)
         now = time.time()
-        until = m.await_args[0][1]
+        until = m.await_args[0][1]  # type: ignore[index]
 
         m.assert_awaited_once()
+        assert jot.idle_timeout is not None
         assert until >= start + jot.leeway + jot.idle_timeout
         assert until <= now + jot.leeway + jot.idle_timeout
 
     @pytest.mark.asyncio
     async def test__exp_no_idle_timeout__until_correct(self, redis_whitelist):
-        jot = Jotbox(
+        jot = Jotbox[Payload](
             encode_key=KEY, leeway=10, expires_in=3500, whitelist=redis_whitelist
         )
         payload = jot.create_payload()
 
         start = time.time()
-        with asyncmock.patch.object(
+        with mock.patch.object(
             jot.whitelist, "add", autospec=True, return_value=None
         ) as m:
             await jot.add_to_whitelist(payload)
         now = time.time()
-        until = m.await_args[0][1]
+        until = m.await_args[0][1]  # type: ignore[index]
 
         m.assert_awaited_once()
+        assert jot.expires_in is not None
         assert until >= math.floor(start + jot.leeway + jot.expires_in)
         assert until <= now + jot.leeway + jot.expires_in
 
     @pytest.mark.asyncio
     async def test__exp_and_idle_timeout_lower__until_correct(self, redis_whitelist):
 
-        jot = Jotbox(
+        jot = Jotbox[Payload](
             encode_key=KEY,
             leeway=10,
             expires_in=3500,
@@ -152,13 +153,14 @@ class TestAddToWhitelist:
         payload = jot.create_payload()
 
         start = time.time()
-        with asyncmock.patch.object(
+        with mock.patch.object(
             jot.whitelist, "add", autospec=True, return_value=None
         ) as m:
             await jot.add_to_whitelist(payload)
         now = time.time()
-        until = m.await_args[0][1]
+        until = m.await_args[0][1]  # type: ignore[index]
 
         m.assert_awaited_once()
+        assert jot.idle_timeout is not None
         assert until >= start + jot.leeway + jot.idle_timeout
         assert until <= now + jot.leeway + jot.idle_timeout
